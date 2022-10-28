f142fe4d4212913a96370cbf90ac02cfbd6dbfad
I-style T <ajantug@gmail.com>
2022-10-28 11:44:40 +0700

    28-10-22-11:44

--- webappmg/settings.py
+++ webappmg/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import django_heroku
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,4 +137,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #เพิ่มใหม่
 
 
-
+django_on_heroku.settings(locals())

1e040679c2ad44d0f84cb8d46491f2d859cd7f07
I-style T <ajantug@gmail.com>
2022-10-31 09:15:13 +0700

    31-10-22-9-15

--- staticfiles/js/checksubmodel.js
+++ staticfiles/js/checksubmodel.js
@@ -1,9 +1,15 @@
-const checksubmodel = document.getElementById('checksubmodel');
-checksubmodel.addEventListener("click", function(e) {
-     const submodel = document.getElementsByName('submodel');
-     
-     if (submodel.value  == "-"){
-          e.preventDefault();
-          document.getElementById('error-queryprice').style.display = 'block';
-     }
+var submodelid = document.getElementById('submodelid');
+const formsubmodel = document.getElementById("formsubmode");
+const errorsubmodel = document.getElementById("errorsubmodel");
+
+formsubmodel.addEventListener("submit", (e)=> {
+                    let box = []
+                    if (submodelid.value === "" || submodelid.value == null) {
+                       box.push('กรุณาเลือกรุ่นย่อย')       
+                    }
+                    if (box.length > 0) {
+                                        e.preventDefault(); 
+                                        errorsubmodel.innerHTML = box.join('');
+                                        document.getElementById("errorsubmodel").className = 'alert alert-danger'; 
+                    }        
 });

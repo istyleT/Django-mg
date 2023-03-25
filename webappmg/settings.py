88c6239a4d1b14bae42e0c498b9f7996612268b1
I-style T <ajantug@gmail.com>
2023-03-25 21:07:20 +0700

    testsetting21.07

--- webappmg/settings.py
+++ webappmg/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import os 
 from os import environ
 import dj_database_url
-import django_on_heroku
+# import django_on_heroku
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -113,14 +113,13 @@ MEDIA_ROOT  = os.path.join(BASE_DIR, 'media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_on_heroku.settings(locals())
+# django_on_heroku.settings(locals())
 
 
-
- #Build paths inside the project like this: BASE_DIR / 'subdir'.
- #Quick-start development settings - unsuitable for production
- #See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
- #SECURITY WARNING: keep the secret key used in production secret
+#Build paths inside the project like this: BASE_DIR / 'subdir'.
+#Quick-start development settings - unsuitable for production
+#See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+#SECURITY WARNING: keep the secret key used in production secret
 ## SECURITY WARNING: don't run with debug turned on in production!
 ## Default primary key field type
 ## https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
